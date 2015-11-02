84d6107abc2fab2e5bc923ef864a12947963776b
emil <emil.mircea30@gmail.com>
2015-11-02 22:40:07 +0100

    initial commit

---
+++ js/app.js
@@ -0,0 +1,77 @@
+var refresh = function() {
+
+  var gl = $('input[name=gl]:checked').val();
+  var el = $('input[name=el]:checked').val();
+  var dh = $('input[name=dh]:checked').val();
+
+  var volume = 'voor extra volume adviseren wij ca. 50-75';
+  var geenExt = 'wij adviseren met deze eigen haarlengte en de gewenste lengte geen';
+  var t = [[
+          [  25,      50,     75],
+          [ volume, volume, volume],
+          [ volume, volume, volume],
+          [ volume, volume, volume]
+        ],[
+          [ 75,       100,      125],
+          [ 50,       75,       100],
+          [ volume,  volume,  volume],
+          [ volume,  volume,  volume]
+        ],[
+          [ geenExt,  geenExt,  geenExt],
+          [ 125,       150,       175],
+          [ 100,       125,       150],
+          [ volume,    volume,   volume]
+        ],[
+          [ geenExt,  geenExt,  geenExt],
+          [ 150,        175,        200],
+          [ 125,        150,        175],
+          [ 100,        125,        150]
+        ],[
+          [ geenExt,  geenExt,  geenExt],
+          [ geenExt, geenExt,   geenExt],
+          [ 150,      175,        200],
+          [ 125,      150,        175]
+        ]
+    ];
+
+  if (dh && gl && el) {
+    var results = t[gl][el][dh];
+    $('p.advies-text').html('Wij adviseren ' + results + ' extensions');
+    $('#firstPk').val(Math.ceil(results /   25 )+ ' packets');
+    $('#secondPk').val(Math.ceil(results /  20 )+ ' packets');
+    $('#thirdPk').val(Math.ceil(results /   25 )+ ' packets');
+    $('#fourthPk').val(Math.ceil(results /  50 )+ ' packets');
+
+
+    if (results === volume) {
+      $('#firstPk').val(3+ ' packets');
+      $('#secondPk').val(4+ ' packets');
+      $('#thirdPk').val(3+ ' packets');
+      $('#fourthPk').val(2+ ' packets');
+    }
+
+    if (results === geenExt) {
+      $('#firstPk').val('not available');
+      $('#secondPk').val('not available');
+      $('#thirdPk').val('not available');
+      $('#fourthPk').val('not available');
+    }
+
+    if (gl == 3) {
+      $('#secondPk').val('not available');
+    }
+
+    if (gl == 4) {
+      $('#secondPk').val('not available');
+      $('#thirdPk').val('not available');
+      $('#fourthPk').val('not available');
+    }
+
+
+  } // end main if statement
+
+  else {
+    $('p.advies-text').html('Please select a value for all options.');
+  }
+
+}; // end function
