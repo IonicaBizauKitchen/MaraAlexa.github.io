787c4f4b137d66eb2936f3895d81eb32fa08b6ca
Ionică Bizău <bizauionica@gmail.com>
2015-11-06 12:31:10 +0200

    Removed the refresh references from HTML
    Also, call the refresh function on page load to fix the back button issue.

--- js/app.js
+++ js/app.js
@@ -1,79 +1,83 @@
-var refresh = function() {
+$(document).ready(function () {
 
-    var gl = $('input[name=gl]:checked').val();
-    var el = $('input[name=el]:checked').val();
-    var dh = $('input[name=dh]:checked').val();
+    function refresh() {
+        var gl = $('input[name=gl]:checked').val();
+        var el = $('input[name=el]:checked').val();
+        var dh = $('input[name=dh]:checked').val();
 
-    var volume = 'voor extra volume ca. 50-75';
-    var geenExt = 'met deze eigen haarlengte en de gewenste lengte GEEN';
+        var volume = 'voor extra volume ca. 50-75';
+        var geenExt = 'met deze eigen haarlengte en de gewenste lengte GEEN';
 
-    var t = [
-        [
-            [ 25, 50, 75 ],
-            [ volume, volume, volume ],
-            [ volume, volume, volume ],
-            [ volume, volume, volume ]
-        ],
-        [
-            [ 75, 100, 125 ],
-            [ 50, 75, 100 ],
-            [ volume, volume, volume ],
-            [ volume, volume, volume ]
-        ],
-        [
-            [ geenExt, geenExt, geenExt ],
-            [ 125, 150, 175 ],
-            [ 100, 125, 150 ],
-            [ volume, volume, volume ]
-        ],
-        [
-            [ geenExt, geenExt, geenExt ],
-            [ 150, 175, 200 ],
-            [ 125, 150, 175 ],
-            [ 100, 125, 150 ]
-        ],
-        [
-            [ geenExt, geenExt, geenExt ],
-            [ geenExt, geenExt, geenExt ],
-            [ 150, 175, 200 ],
-            [ 125, 150, 175 ]
-        ]
-    ];
+        var t = [
+            [
+                [ 25, 50, 75 ],
+                [ volume, volume, volume ],
+                [ volume, volume, volume ],
+                [ volume, volume, volume ]
+            ],
+            [
+                [ 75, 100, 125 ],
+                [ 50, 75, 100 ],
+                [ volume, volume, volume ],
+                [ volume, volume, volume ]
+            ],
+            [
+                [ geenExt, geenExt, geenExt ],
+                [ 125, 150, 175 ],
+                [ 100, 125, 150 ],
+                [ volume, volume, volume ]
+            ],
+            [
+                [ geenExt, geenExt, geenExt ],
+                [ 150, 175, 200 ],
+                [ 125, 150, 175 ],
+                [ 100, 125, 150 ]
+            ],
+            [
+                [ geenExt, geenExt, geenExt ],
+                [ geenExt, geenExt, geenExt ],
+                [ 150, 175, 200 ],
+                [ 125, 150, 175 ]
+            ]
+        ];
 
-    if (dh && gl && el) {
-        var results = t[gl][el][dh];
-        $('p.advies-text').html('Wij adviseren ' + results + ' extensions');
+        if (dh && gl && el) {
+            var results = t[gl][el][dh];
+            $('p.advies-text').html('Wij adviseren ' + results + ' extensions');
 
-        $('#firstPk').val(Math.ceil(results / 25) + ' packets');
-        $('#secondPk').val(Math.ceil(results / 20) + ' packets');
-        $('#thirdPk').val(Math.ceil(results / 25) + ' packets');
-        $('#fourthPk').val(Math.ceil(results / 50) + ' packets');
+            $('#firstPk').val(Math.ceil(results / 25) + ' packets');
+            $('#secondPk').val(Math.ceil(results / 20) + ' packets');
+            $('#thirdPk').val(Math.ceil(results / 25) + ' packets');
+            $('#fourthPk').val(Math.ceil(results / 50) + ' packets');
 
-        if (results === volume) {
-            $('#firstPk').val(3 + ' packets');
-            $('#secondPk').val(4 + ' packets');
-            $('#thirdPk').val(3 + ' packets');
-            $('#fourthPk').val(2 + ' packets');
+            if (results === volume) {
+                $('#firstPk').val(3 + ' packets');
+                $('#secondPk').val(4 + ' packets');
+                $('#thirdPk').val(3 + ' packets');
+                $('#fourthPk').val(2 + ' packets');
+            }
+
+            var notAvailable = [];
+            if (gl == 3) {
+                notAvailable.push('#secondPk')
+            }
+
+            if (gl == 4) {
+                notAvailable.push('#thirdPk', '#fourthPk');
+            }
+
+            $(notAvailable.join(",")).val('not available');
+
+            if (results === geenExt) {
+                $('#firstPk,#secondPk,#thirdPk,#fourthPk').val('');
+            }
+
+        } // end main if statement
+        else {
+            $('p.advies-text').html('Please select a value for all options.');
         }
-
-        var notAvailable = [];
-        if (gl == 3) {
-            notAvailable.push('#secondPk')
-        }
-
-        if (gl == 4) {
-            notAvailable.push('#thirdPk', '#fourthPk');
-        }
-
-        $(notAvailable.join(",")).val('not available');
-
-        if (results === geenExt) {
-            $('#firstPk,#secondPk,#thirdPk,#fourthPk').val('');
-        }
-
-    } // end main if statement
-    else {
-        $('p.advies-text').html('Please select a value for all options.');
     }
 
-}; // end function
+    $(".box-buttons > input[type=radio]").on("click", refresh);
+    refresh();
+});
